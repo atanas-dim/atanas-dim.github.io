7e6d25989e0f95c81190d3a18a57484af072fb63
atanasdim <hi.atanasdim@gmail.com>
2021-04-07 17:45:24 +0100

    lightbox memory leak fixed

--- js/lightbox.js
+++ js/lightbox.js
@@ -6,7 +6,10 @@ const modalImage = document.querySelector(".modal-image");
 let imageElement;
 let imageCurrentWidth;
 let imageMaxWidth;
-let timesClicked = 0;
+let imageCurrentHeight;
+let imageMaxHeight;
+let isAtMaxSize;
+let isZoomed = false;
 
 // Create img element, add attributes to it and append it to the image container
 function createImage (src, alt) {
@@ -16,32 +19,42 @@ function createImage (src, alt) {
     imageElement = modalImage.appendChild(image);
 }
 
-// Saves the widths of the image container and the image element, which we use later 
-// to check if the image can be zoomed and also to update the cursor.
-function updateImageWidths () {
+// Saves the widths and heights of the image container and the image element, which we use later 
+// to check if the image is at its maximun size and also to update the cursor.
+function updateImageSize () {
     imageCurrentWidth = modalImage.offsetWidth;
     imageMaxWidth = imageElement.naturalWidth;
+    imageCurrentHeight = modalImage.offsetHeight;
+    imageMaxHeight = imageElement.naturalHeight;
 }
 
-// Fuction which updates the cursor on the lighbox image container depending of the size displayed.
-// If the image is already at its max size/width and can't be zoomed, then cursor is auto.
-// If the image can be zoomed in and then zoomed out, then the cursor is a pointer.
+// Function check if image is displayed at its natural/maximum size or not
+function checkMaxSize () {
+    if (imageMaxWidth <= imageCurrentWidth + 20 && imageMaxHeight <= imageCurrentHeight + 20) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Fuction which updates the cursor on the lighbox image container depending of the size displayed and if it has been zoomed already.
 function updateCursor () {
-    if (imageMaxWidth <= imageCurrentWidth && timesClicked !== 2) {
+    if ( isAtMaxSize && !isZoomed) {
         modalImage.style.cssText = "cursor: auto";
-    } else if (imageMaxWidth > imageCurrentWidth || timesClicked === 2) {
+    } else {
         modalImage.style.cssText = "cursor: pointer";
     }
 }
 
-// On image resize the observer updates the widths which we use to decide if the image can be zoomed in or not. Also calls the cursor update
+// On image resize the observer updates the widths/heights which we use to check if the image is at maximum size. Also calls the cursor update
 const observer = new ResizeObserver(entry => {
-    updateImageWidths();
-    updateCursor();    
+    updateImageSize();
+    isAtMaxSize =  checkMaxSize();   
+    updateCursor();
 });
 
 // Function to make the lighbox container visible. It creates an image in it with the src and alt of the original/target image which we clicked.
-// This function also starts the resize observer to help the zoom function use the right widths of the image dispalyed.
+// This function also starts the resize observer to help the zoom function use the right widths/heigts of the image dispalyed.
 function openLightbox (event) {
     event.preventDefault();
     
@@ -55,6 +68,7 @@ function openLightbox (event) {
     imageElement.classList.add("fit-window");
     document.body.style.overflow = "hidden";
     modal.style.display = "block";
+
 }
 
 // The openLightbox function is used on all gallery links/items
@@ -74,28 +88,27 @@ modalClose.addEventListener("click", closeLightbox);
 modalBg.addEventListener("click", closeLightbox);
 
 
-// Use counter to check how many times the image has been clicked.
-// Then use statements to adjust the zoom/width of the image (which creates zoom in/out feature).
-// On first click the image takes the width from the 'fit-window' css class. On the second click it 
-// takes the width from the 'natural-size' css class (defined in lightbox.css)
-// Here we use the maximum/natural width of the target image and the current width of the image container 
-// to check if the image has been already displayed at its max size. If yes, then the counter is reversed to 0 value to disable zoom in.
+// Using isZoomed and isAtMaxSize variables to create conditions.
+// Each condition updates the sizes of the image container and image element with css classes from lightbox.css (which creates the zoom in/out feature).
+// When the lightbox first opens, the image takes the width from the 'fit-window' css class from lightbox.css by default. 
+// On first click(zoom in) it takes the width from the 'natural-size' css class and the isZoomed variable is updated.
+// On second click (zoom out) the isZoomed variable is reversed, so that it toggles the css classes of the image container and image element.
 function zoom () {
-
-    timesClicked++;
-
-    if (timesClicked === 1 && imageMaxWidth <= imageCurrentWidth + 10) {
-        timesClicked = 0;
-    } else if (timesClicked === 1 && imageMaxWidth > imageCurrentWidth + 10) {
+    
+    if (!isZoomed && isAtMaxSize) {
+        return;
+    } else if (!isZoomed && !isAtMaxSize) {
         modalImage.classList.add("zoom-full-size");
         imageElement.classList.remove("fit-window");
         imageElement.classList.add("natural-size"); 
-    } else if (timesClicked === 2) {
+        isZoomed = true;
+    } else if (isZoomed) {
         modalImage.classList.remove("zoom-full-size");
         imageElement.classList.remove("natural-size");
         imageElement.classList.add("fit-window");
-        timesClicked = 0;
+        isZoomed = false;
     };
+
 }
 
 modalImage.addEventListener("click", zoom);
