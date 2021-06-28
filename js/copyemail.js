980d25dc33e60606ea84f2d1d791bd5e067c3794
atanasdim <hi.atanasdim@gmail.com>
2021-06-28 13:29:35 +0100

    Copy email to clipboard reworked without JQuery

--- js/copyemail.js
+++ js/copyemail.js
@@ -1,47 +1,62 @@
-// COPY EMAIL TO CLIPBOARD
+const emailLinks = Array.from(document.querySelectorAll("a[href^=mailto]"));
 
-// Disable default link function
-$('a[href^=mailto]').on('click', function() {
-  return false
-})
+// On click run copy email and show alert functions
+emailLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    copyEmail(link);
+    showAlert(link);
+  });
+});
 
-// Copy to clipboard function found on Stack Overflow
-function copyToClipboard(text) {
-var $temp = $("<input>");
-$("body").append($temp);
-$temp.val(text).select();
-document.execCommand("copy");
-$temp.remove();
+// Copy text to clipboard
+function copyEmail(element) {
+  const href = element.href;
+  const textToCopy = href.replace("mailto:", "");
+
+  const myTemporaryInputElement = document.createElement("input");
+  myTemporaryInputElement.type = "text";
+  myTemporaryInputElement.value = textToCopy;
+
+  document.body.appendChild(myTemporaryInputElement);
+
+  myTemporaryInputElement.select();
+  document.execCommand("Copy");
+
+  document.body.removeChild(myTemporaryInputElement);
 }
 
+// Show/hide alert message
+function showAlert(element) {
+  const alertDiv = document.createElement("div");
+  alertDiv.classList.add("email-alert");
+  alertDiv.innerHTML = "Email copied";
 
-// Function to show alert box on hover
-// Followed an example on CodePen from Eric Clark
-$(document).ready(function() {
+  element.style.position = "relative";
 
-$('a[href^=mailto]').addClass('email-link');
+  element.appendChild(alertDiv);
 
-var messageCopied = 'Email copied';
+  const fadeIn = setInterval(function () {
+    if (!alertDiv.style.opacity) {
+      alertDiv.style.opacity = 0;
+    }
+    if (alertDiv.style.opacity < 1) {
+      alertDiv.style.opacity = 1;
+    } else {
+      clearInterval(fadeIn);
+    }
+  }, 0);
 
-// Add a div inside the link to hold the alert text. Keep it empty to be able to 
-// switch messages without any text staying before the message.
-$('.email-link').append('<div class="email-alert"></div>');
+  const fadeOut = setInterval(function () {
+    if (alertDiv.style.opacity > 0) {
+      alertDiv.style.opacity = 0;
+    } else {
+      clearInterval(fadeOut);
+    }
+  }, 1500);
 
-// Add the text to the alert div
-$('.email-alert').append(messageCopied);
-
-// Function to get the email from the href of the link and copy to clipboard
-$('.email-link').on('click', function() {
-
-    var href = $(this).attr('href');
-    var email = href.replace('mailto:', '');
-
-    copyToClipboard(email);
-
-    $(this).find('.email-alert').fadeIn(200);
-    setTimeout(function() {
-        $('.email-alert').fadeOut(200);}, 1200);         
-
-})
-
-})
+  // remove the alert div
+  setTimeout(function () {
+    alertDiv.remove();
+  }, 1600);
+}
